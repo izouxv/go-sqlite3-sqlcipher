5948cef215536fc8e71e2881e7f2402a2251e2e8
zouxu <izouxv@gmail.com>
2025-03-27 20:17:47 +0800

    add test data

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mattn/go-sqlite3
 
 go 1.16
+
+require github.com/stretchr/testify v1.10.0
--- sqlcipher.go
+++ sqlcipher.go
@@ -3,8 +3,6 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-// +build sqlcipher
-
 package sqlite3
 
 /*
@@ -17,3 +15,36 @@ package sqlite3
 #cgo darwin CFLAGS: -DSQLCIPHER_CRYPTO_CC
 */
 import "C"
+import (
+	"bytes"
+	"errors"
+	"os"
+)
+
+// sqlite3Header defines the header string used by SQLite 3.
+var sqlite3Header = []byte("SQLite format 3\000")
+
+// IsEncrypted returns true, if the database with the given filename is
+// encrypted, and false otherwise.
+// If the database header cannot be read properly an error is returned.
+func IsEncrypted(filename string) (bool, error) {
+	// open file
+	db, err := os.Open(filename)
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+	// read header
+	var header [16]byte
+	n, err := db.Read(header[:])
+	if err != nil {
+		return false, err
+	}
+	if n != len(header) {
+		return false, errors.New("go-sqlcipher: could not read full header")
+	}
+	// SQLCipher encrypts also the header, the file is encrypted if the read
+	// header does not equal the header string used by SQLite 3.
+	encrypted := !bytes.Equal(header[:], sqlite3Header)
+	return encrypted, nil
+}
